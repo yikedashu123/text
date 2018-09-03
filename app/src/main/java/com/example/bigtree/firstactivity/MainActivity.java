f5670ef95093f6d33c10e91d26e995811813c384
BigTtee <2404813996@qq.com>
2018-09-03 19:10:08 +0800

    first commit

---
+++ app/src/main/java/com/example/bigtree/firstactivity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bigtree.firstactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+* 作者：一棵大树
+* 日期：2018/9/3 18:28
+* 包名：com.example.bigtree.firstactivity
+*/
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
